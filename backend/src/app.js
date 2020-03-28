7905d91363181a3ec86f7ea650d59268227cc88a
André Pires <andrebeckpires@gmail.com>
2020-03-28 17:34:35 -0300

    testes
    criando ambiente de test

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');//importa o modulo express pra variavel express
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 const app = express(); //instancia a aplicação
 
@@ -7,12 +8,13 @@ const app = express(); //instancia a aplicação
 app.use(cors());
 app.use(express.json());//pra declarar q usa o json para requisições
 app.use(routes);
+app.use(errors());
 
 
  
 
 
 
-app.listen(3333);//local host usando 3333
+module.exports = app;
 
 
